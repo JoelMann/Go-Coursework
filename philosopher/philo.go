a3d93fd1961c6e2587fe3e05c41d5633061ec923
Joel Mann <jmann.creston@gmail.com>
2022-04-04 13:53:15 +0000

    Implemented an arbitrator

--- philosopher/philo.go
+++ philosopher/philo.go
@@ -20,8 +20,69 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
+
+type Chopstick struct {
+	mu sync.Mutex
+}
+
+type Philosopher struct {
+	number         int
+	leftChopstick  *Chopstick
+	rightChopstick *Chopstick
+}
+
+type Host struct {
+	numberOfEating int
+	request        sync.Mutex
+}
+
+func (h *Host) AskForPermission() bool {
+	h.request.Lock()
+	if h.numberOfEating < 2 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (p Philosopher) ImDoneEatingMom(god *Host) {
+	god.numberOfEating -= 1
+	god.request.Unlock()
+}
+
+func (p Philosopher) EatRice(god *Host) {
+	if god.AskForPermission() {
+		god.numberOfEating += 1
+		p.leftChopstick.mu.Lock()
+		p.rightChopstick.mu.Lock()
+		fmt.Printf("Philosopher %d is eating\n", p.number)
+		p.leftChopstick.mu.Unlock()
+		p.rightChopstick.mu.Unlock()
+		p.ImDoneEatingMom(god)
+	}
+}
 
 func main() {
 	fmt.Println("sanity test")
+	var groupOfPhilosophers []Philosopher
+	chopsticks := make([]Chopstick, 5)
+	bob := Host{numberOfEating: 0}
+
+	for i := 0; i < 5; i++ {
+		phil := Philosopher{
+			number:         i + 1,
+			leftChopstick:  &chopsticks[(i+5)%5],
+			rightChopstick: &chopsticks[(i+1)%5],
+		}
+		groupOfPhilosophers = append(groupOfPhilosophers, phil)
+	}
+
+	for {
+		for i := 0; i < 5; i++ {
+			go groupOfPhilosophers[i].EatRice(&bob)
+		}
+	}
+
 }
