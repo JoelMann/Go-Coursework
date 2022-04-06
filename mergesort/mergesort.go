d088ff949eac0c8cfdadeb418de1be286502f5f1
Joel Mann <jmann.creston@gmail.com>
2022-04-06 14:32:33 +0000

    Boilerplate to work off of

---
+++ mergesort/mergesort.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func splitSort(subsection []int) []int {
+	return subsection
+}
+
+func merge(sectionLeft []int, sectionRight []int) []int {
+	sortedSection := make([]int, len(sectionLeft)+len(sectionRight))
+
+	return sortedSection
+}
+
+func main() {
+	fmt.Println("Sanity Test")
+}
